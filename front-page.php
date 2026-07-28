d1ba86c4d116a35134186c4be20dd3dde544d870
Ilaria Roglieri <ilariaroglieri@gmail.com>
2026-07-28 18:39:35 +0200

    ongoing exhibition in homepage

--- front-page.php
+++ front-page.php
@@ -1,25 +1,72 @@
 <?php get_header(); ?>
 
-<main id="content-home" class="content d-flex d-column space-between grow">
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<main>
+  <?php if ( have_posts() ) : ?>
+    <div id="content-home" class="d-flex d-column space-between grow">
+      <?php  while ( have_posts() ) : the_post(); ?>
 
-    <div class="intro-text">
-      <?php the_content(); ?>
-    </div>
+        <div class="intro-text">
+          <?php the_content(); ?>
+        </div>
 
-    <div class="current-exhibition">
-      Ongoing<br/>
-      September 12 -- December 18, 2026 <br/>
-      Personalissime Sculture 
-  
+        <?php // current exhibition 
+        $args = [
+          'post_type' => 'post',
+          'posts_per_page' => -1,
+        ];
+
+        $exhibitions = new WP_Query($args);
+        $ongoing = null;
+        $today = new DateTime(date('Y-m-d'), new DateTimeZone('UTC')); 
+
+        while ($exhibitions->have_posts()): $exhibitions->the_post();
+          $startD = DateTime::createFromFormat('Ymd', get_field('starting_date', false, false));
+          $endD = DateTime::createFromFormat('Ymd', get_field('ending_date', false, false));
+
+          $startY = $startD->format('Y'); 
+          $endY = $endD->format('Y');
+
+          $startM = $startD->format('m');
+          $endM = $endD->format('m');
+
+          $cleanStartD = null;
+          if ($startY == $endY) {
+            if ($startM == $endM) {
+              $cleanStartD = $startD->format('j');
+            } else {
+              $cleanStartD = date_i18n('j F', $startD->getTimestamp());
+            }
+          } else {
+            $cleanStartD = date_i18n('j F Y', $startD->getTimestamp());
+          }
+
+          $cleanEndD = date_i18n('j F Y', $endD->getTimestamp());
+
+
+          if ($startD <= $today && $today <= $endD): ?>
+
+            <div class="current-exhibition">
+              <p>Ongoing</p>
+              <p><?= $cleanStartD . '–' . $cleanEndD; ?></p>
+              <h2 class="latest-ex-title"><?php the_title(); ?></h2>
+              <h3 class="latest-ex-artists"><?= get_field('artist'); ?></h3>
+            </div>
+            
+          <?php break; // takes the first
+          endif;
+        endwhile;
+        wp_reset_postdata(); ?>
+
+      
+      <?php endwhile; ?>
     </div>
-  
-  <?php endwhile; else: ?>
+  <?php else: ?>
 
     <h2>Woops...</h2>
     <p>Sorry, no posts found.</p>
 
   <?php endif; ?>
+
 </main>
 
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,43 +1,49 @@
 <?php get_header(); ?>
 
-<main class="content" id="content-exhibitions">
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
+<main>
+  <?php if ( have_posts() ) : ?>
     <?php 
       $today = new DateTime(date('Y-m-d'), new DateTimeZone('UTC'));
-      $startD = DateTime::createFromFormat('Ymd', get_field('starting_date', false, false));
-      $endD = DateTime::createFromFormat('Ymd', get_field('ending_date', false, false));
-
-      $status = '';
-      $file = get_field('pdf_file');
-
-      if ($startD <= $today && $today <= $endD):
-        $status = 'ongoing';
-      elseif ($startD > $today):
-        $status = 'incoming';
-      elseif ($endD < $today):
-        $status = 'ended';
-      endif;
     ?>
+    <div id="content-exhibitions">
+      <?php while ( have_posts() ) : the_post(); ?>
 
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> data-status="<?= $status ?>">
-      <div class="exhibition-row d-flex flex-row">
-        <div class="d-3-twelfth">
-          <h2 class="entry-cat"><?php $status == 'ongoing' ? 'Ongoing / ' : '' ?>Categoria</h2>
-        </div>
-        <div class="d-3-twelfth">
-          <h3 class="entry-title"><?php the_title(); ?></h3>
-        </div>
-        <div class="d-3-twelfth">
-          <h3 class="entry-artists"><?= get_field('artist'); ?></h3>
-        </div>
-        <div class="d-3-twelfth">
-          <a href="<?= $file['url'] ?>" class="entry-download">Download PDF</a>
-        </div>
-      </div>
-    </article>
-  
-  <?php endwhile; else: ?>
+        <?php 
+          $startD = DateTime::createFromFormat('Ymd', get_field('starting_date', false, false));
+          $endD = DateTime::createFromFormat('Ymd', get_field('ending_date', false, false));
+
+          $status = '';
+          $file = get_field('pdf_file');
+          $cats = get_the_category();
+
+          if ($startD <= $today && $today <= $endD):
+            $status = 'ongoing';
+          elseif ($startD > $today):
+            $status = 'incoming';
+          elseif ($endD < $today):
+            $status = 'ended';
+          endif;
+        ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> data-status="<?= $status ?>">
+          <div class="exhibition-row d-flex flex-row">
+            <div class="d-3-twelfth">
+              <h2 class="entry-cat"><?= $status == 'ongoing' ? 'Ongoing / ' : '' ?><?= implode( ', ', array_column( $cats, 'name' ) ); ?></h2>
+            </div>
+            <div class="d-3-twelfth">
+              <h3 class="entry-title"><?php the_title(); ?></h3>
+            </div>
+            <div class="d-3-twelfth">
+              <h3 class="entry-artists"><?= get_field('artist'); ?></h3>
+            </div>
+            <div class="d-3-twelfth">
+              <a href="<?= $file['url'] ?>" class="entry-download">Download PDF</a>
+            </div>
+          </div>
+        </article>
+      <?php endwhile; ?>
+    </div>
+  <?php else: ?>
 
     <h2>Woops...</h2>
     <p>Sorry, no posts found.</p>
