161916fca201c8983bfa630cdc200a1110e4a09b
Ilaria Roglieri <ilariaroglieri@gmail.com>
2026-07-28 18:04:41 +0200

    working on posts page

--- archive.php
+++ archive.php
@@ -3,15 +3,38 @@
 <main class="content" id="content-archive">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-      <h2 class="entry-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-      <?php if ( has_post_thumbnail() ) : ?>  
-        <a  href="<?php the_permalink() ?>">          
-          <?php the_post_thumbnail(); ?>
-        </a>
+    <?php 
+      $today = new DateTime(date('Y-m-d'));
+      $startD = new DateTime(get_field('starting_date', false, false));
+      $endD = new DateTime(get_field('ending_date', false, false));
+      $status = '';
+      $file = get_field('pdf_file');
+
+      if ($startD <= $today && $today <= $endD):
+        $status = 'ongoing';
+      elseif ($startD > $today):
+        $status = '';
+      elseif ($endD < $today):
+        $status = '';
+      endif;
+    ?>
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> data-status="<?= $status ?>">
+      <div class="exhibition-row" class="d-flex flex-row">
+        <div class="d-3-twelfth">
+          <h2 class="entry-cat"><?= $status ?> / Categoria</h2>
+        </div>
+        <div class="d-3-twelfth">
+          <h3 class="entry-title"><?php the_title(); ?></h3>
+        </div>
+        <div class="d-3-twelfth">
+          <h3 class="entry-artists"><?= get_field('artist'); ?></h3>
+        </div>
+        <div class="d-3-twelfth">
+          <a href="<?= $file['url'] ?>" class="entry-download">Download PDF</h3>
+        </div>
     </article>
   
-  <?php endif; ?>
   <?php endwhile; else: ?>
 
     <h2>Woops...</h2>
--- inc/cpt.php
+++ inc/cpt.php
@@ -3,29 +3,49 @@
   function rename_default_post_label() {
     global $menu;
     global $submenu;
-    $menu[5][0] = 'Projects';
-    $submenu['edit.php'][5][0] = 'Projects';
-    $submenu['edit.php'][10][0] = 'Add Project';
-    $submenu['edit.php'][16][0] = 'Projects Tags';
+    $menu[5][0] = 'Exhibitions';
+    $submenu['edit.php'][5][0] = 'Exhibitions';
+    $submenu['edit.php'][10][0] = 'Add Exhibition';
+    $submenu['edit.php'][16][0] = 'Exhibitions Tags';
   }
   function rename_default_post_object() {
     global $wp_post_types;
     $labels = &$wp_post_types['post']->labels;
-    $labels->name = 'Projects';
-    $labels->singular_name = 'Project';
-    $labels->add_new = 'Add Project';
-    $labels->add_new_item = 'Add Project';
-    $labels->edit_item = 'Edit Project';
-    $labels->new_item = 'Projects';
-    $labels->view_item = 'View Projects';
-    $labels->search_items = 'Search Projects';
-    $labels->not_found = 'No Projects found';
-    $labels->not_found_in_trash = 'No Projects found in Trash';
-    $labels->all_items = 'All Projects';
-    $labels->menu_name = 'Projects';
-    $labels->name_admin_bar = 'Projects';
+    $labels->name = 'Exhibitions';
+    $labels->singular_name = 'Exhibition';
+    $labels->add_new = 'Add Exhibition';
+    $labels->add_new_item = 'Add Exhibition';
+    $labels->edit_item = 'Edit Exhibition';
+    $labels->new_item = 'Exhibitions';
+    $labels->view_item = 'View Exhibitions';
+    $labels->search_items = 'Search Exhibitions';
+    $labels->not_found = 'No Exhibitions found';
+    $labels->not_found_in_trash = 'No Exhibitions found in Trash';
+    $labels->all_items = 'All Exhibitions';
+    $labels->menu_name = 'Exhibitions';
+    $labels->name_admin_bar = 'Exhibitions';
   }
    
   add_action( 'admin_menu', 'rename_default_post_label' );
   add_action( 'init', 'rename_default_post_object' );
+
+
+  $args = [
+    'post_type'      => 'exhibition',
+    'posts_per_page' => -1,
+    'meta_key'       => 'starting_date',
+    'orderby'        => 'meta_value',
+    'order'          => 'ASC',
+  ];
+
+  function orderExhibitions( $query ) {
+    if ( ! is_admin() && $query->is_main_query() && is_home() ) {
+      $query->set( 'posts_per_page', -1 );
+      $query->set( 'meta_key', 'starting_date');
+      $query->set( 'orderby','meta_value');
+      $query->set( 'order','DESC');
+      return;
+    }
+  }
+  add_action( 'pre_get_posts', 'orderExhibitions', 1 );
 ?>
