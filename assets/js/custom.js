d2283c4ce35bdc474fe895bcf7a4def63e55812a
Ilaria Roglieri <ilariaroglieri@gmail.com>
2026-07-28 17:20:17 +0200

    animation + dynamic page load

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,69 @@
+const pos = ['pos-1', 'pos-2', 'pos-3', 'pos-4'];
+const w = ['w-1', 'w-2', 'w-3', 'w-4'];
+const bars = document.querySelectorAll('.overlay-bar');
+const content = document.querySelector('main');
 
+let currentClasses = [
+  { pos: null, w: null },
+  { pos: null, w: null }
+];
+
+function pickDifferent(arr, exclude) {
+  const available = arr.filter(c => c !== exclude);
+  return available[Math.floor(Math.random() * available.length)];
+}
+
+function applyPositions() {
+  // posizioni: diverse tra le due barre e diverse dalle precedenti
+  const newPos0 = pickDifferent(pos, currentClasses[0].pos);
+  const newPos1 = pickDifferent(pos.filter(p => p !== newPos0), currentClasses[1].pos);
+
+  // larghezze: diverse dalle precedenti, ma possono coincidere tra le due barre
+  const newW0 = pickDifferent(w, currentClasses[0].w);
+  const newW1 = pickDifferent(w, currentClasses[1].w);
+
+  const next = [
+    { pos: newPos0, w: newW0 },
+    { pos: newPos1, w: newW1 }
+  ];
+
+  bars.forEach((bar, i) => {
+    // rimuovi classi precedenti
+    bar.classList.remove(...pos, ...w);
+    bar.classList.add(next[i].pos, next[i].w);
+  });
+
+  currentClasses = next;
+}
+
+// posizioni iniziali immediate
+applyPositions();
+
+async function navigateTo(url) {
+  applyPositions();
+  // content.classList.remove('fadein');
+
+  const response = await fetch(url);
+  const html = await response.text();
+  const parser = new DOMParser();
+  const newDoc = parser.parseFromString(html, 'text/html');
+
+  // await new Promise(resolve => setTimeout(resolve, 500));
+  // content.classList.add('fadein');
+
+  document.title = newDoc.title;
+  content.innerHTML = newDoc.querySelector('main').innerHTML;
+  history.pushState({}, '', url);
+  
+}
+
+// intercetta i link del menu
+document.querySelector('.menu-menu-1-container ul').addEventListener('click', e => {
+  const link = e.target.closest('a');
+  if (!link) return;
+  e.preventDefault();
+  navigateTo(link.href);
+});
+
+// loading content
+content.classList.add('loaded');
--- footer.php
+++ footer.php
@@ -1,5 +1,13 @@
 	<footer>
 	</footer>
+
+	<div id="overlay-container" class="p-fixed full-height full-width">
+		<div class="d-flex flex-row full-height">
+			<div class="overlay-bar"></div>
+			<div class="overlay-bar"></div>
+		</div>
+	</div>
+
 </div>
 
 <?php wp_footer(); ?>
